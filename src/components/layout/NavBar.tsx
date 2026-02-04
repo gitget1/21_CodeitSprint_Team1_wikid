72e67735915af0722b0c85be8e5fee380a1c3b75
zzungju <zzungju07@gmail.com>
2026-02-04 23:54:18 +0900

    :sparkles:feat:내브바 메뉴 토글 및 외부 클릭 닫기 구현

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 
 import { BellIcon } from '@/assets/icons/Bell';
@@ -6,11 +6,53 @@ import { UserIcon } from '@/assets/icons/User';
 import { MenuIcon } from '@/assets/icons/Menu';
 import Logo from '@/assets/images/logo.png';
 
+import Menu from '../ui/Menu';
+
 const HOVER_GREEN = 'hover:text-primary-green-300';
 
 function Navbar() {
-  //임시 로그인 상태
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  //상태별 메뉴 목록 정의
+  const guestMenu = [
+    { label: '위키목록', onClick: () => {} },
+    { label: '자유게시판', onClick: () => {} },
+    { label: '로그인', onClick: () => {} },
+  ];
+
+  const userMenu = [
+    { label: '위키목록', onClick: () => {} },
+    { label: '자유게시판', onClick: () => {} },
+    { label: '알림', onClick: () => {} },
+    { label: '마이페이지', onClick: () => {} },
+  ];
+
+  const profileMenu = [
+    { label: '계정설정', onClick: () => {} },
+    { label: '내위키', onClick: () => {} },
+    { label: '로그아웃', onClick: () => setIsLoggedIn(false) },
+  ];
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false); //임시 로그인 상태
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openProfileMenu, setOpenProfileMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const profileRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as Node;
+      if (menuRef.current && !menuRef.current.contains(target)) {
+        setOpenMenu(false);
+      }
+
+      if (profileRef.current && !menuRef.current?.contains(target)) {
+        setOpenProfileMenu(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="h-15 xl:h-20 px-5 lg:px-20 py-4 flex justify-between items-center">
@@ -27,7 +69,7 @@ function Navbar() {
         <div className="hidden md:flex items-center">
           {!isLoggedIn && (
             <button
-              onClick={() => setLoggedIn(true)} //임시 로그인 완료
+              onClick={() => setIsLoggedIn(true)} //임시 로그인 완료
               className={`text-gray-400 text-md-regular ${HOVER_GREEN}`}
             >
               로그인
@@ -36,13 +78,23 @@ function Navbar() {
           {isLoggedIn && (
             <div className=" flex justify-center items-center gap-6">
               <BellIcon className={`text-gray-400 cursor-pointer text-md-regular ${HOVER_GREEN}`} />
-              <UserIcon className={`text-gray-400 cursor-pointer text-md-regular ${HOVER_GREEN}`} />
+              <div className="relative" ref={profileRef}>
+                <UserIcon
+                  onClick={() => setOpenProfileMenu(!openProfileMenu)}
+                  className={`text-gray-400 cursor-pointer text-md-regular ${HOVER_GREEN}`}
+                />
+                {openProfileMenu && <Menu items={profileMenu} />}
+              </div>
             </div>
           )}
         </div>
-        <MenuIcon
-          className={`md:hidden block text-gray-400 cursor-pointer text-md-regular ${HOVER_GREEN}`}
-        />
+        <div className="relative md:hidden" ref={menuRef}>
+          <MenuIcon
+            onClick={() => setOpenMenu(!openMenu)}
+            className={`block text-gray-400 cursor-pointer text-md-regular ${HOVER_GREEN}`}
+          />
+          {openMenu && <Menu items={isLoggedIn ? userMenu : guestMenu} />}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ui/Menu.tsx
@@ -0,0 +1,34 @@
+interface MenuItem {
+  label: string;
+  onClick?: () => void;
+}
+
+interface MenuProps {
+  items: MenuItem[];
+}
+
+function Menu({ items }: MenuProps) {
+  return (
+    <div className="w-30 bg-gray-50 rounded-[10px] absolute top-9.25 right-0 shadow-lg overflow-hidden">
+      {items.map((item) => (
+        <button
+          key={item.label}
+          className="
+            py-2.5 w-full
+            flex m-auto
+            justify-center
+            items-center
+            cursor-pointer
+            hover:bg-gray-100
+            text-md-regular
+            text-gray-500
+            hover:text-primary-green-300"
+        >
+          {item.label}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Menu;
