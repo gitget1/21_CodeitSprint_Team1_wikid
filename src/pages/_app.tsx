9b437c56d9e3198c5356144fb679ce5cbe13f77b
zzungju <zzungju07@gmail.com>
2026-02-06 16:56:16 +0900

    :wrench:Chore: Tanstack Query 초기 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
 import '@/styles/globals.css';
 
 import PageLayout from '@/components/layout/PageLayout';
 
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000, // 1분
+            retry: 1,
+          },
+        },
+      })
+  );
+
   return (
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
+    <QueryClientProvider client={queryClient}>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
