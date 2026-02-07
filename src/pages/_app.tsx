b23e234f975c9e3ac14f114cad3fe80d058e181b
kim minseong <msjw9022@naver.com>
2026-02-07 14:54:38 +0900

    _app.tsx develop머지

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,6 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
-
 import '@/styles/globals.css';
 
-import PageLayout from '@/components/layout/PageLayout';
-
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 60 * 1000, // 1분
-            retry: 1,
-          },
-        },
-      })
-  );
-
-  return (
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
-  );
+  return <Component {...pageProps} />;
 }
