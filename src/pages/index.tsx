7443225546649f718c4b9ed076c2af9e558c5e6f
leehi9558 <leehi9558@gmail.com>
2026-02-09 12:59:19 +0900

    ✨ Feat: [ECS-29] 메인랜딩페이지 ui-CtaSection (#24)
    * 🐛 Fix: [ECS-24] index.tsx 충돌 해결 및 병합
    
    * 🚧 Feat: 🚧 충돌 체크를 위한 임시 커밋
    
    * 🚧 Fix: 아이콘 및 스토어코드 충돌 해결 중
    
    * ✨ Feat: [ECS-29] 메인랜딩페이지 ui-CtaSection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
+import CtaSection from "@/components/Home/CtaSection";
+import ViewSection from "@/components/Home/ViewSection";
+
 export default function Home() {
-  return;
+  return (
+    <div className="w-full">
+      <ViewSection />
+      <CtaSection />
+    </div>
+  );
 }
 
