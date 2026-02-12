8c979d43c6a99b75f9ac3a560d8de90b4d89b6d7
zzungju <zzungju07@gmail.com>
2026-02-12 23:13:26 +0900

    :bug:Fix:build 에러- 타입에러 수정

--- src/hooks/useBoard.ts
+++ src/hooks/useBoard.ts
@@ -12,7 +12,12 @@ export default function useArticles() {
       setLoading(true);
       setError(null);
 
-      const data = await boardApi.getArticles({ pageSize: 1000 });
+      const data = await boardApi.getArticles({
+        page: 1,
+        pageSize: 10,
+        orderBy: 'recent',
+        keyword: '',
+      });
       setArticles(data);
     } catch (error) {
       const e = error as AxiosError<{ message: string }>;
