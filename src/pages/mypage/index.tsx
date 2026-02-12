980ecc0192e9f08b01cf4ccdf365cdbd2511f861
zzungju <zzungju07@gmail.com>
2026-02-12 22:49:48 +0900

    :art:Style:비밀번호변경 UI 구현

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,38 +1,70 @@
-import React, { useState } from 'react';
-
-import Dropdown from '@/components/ui/Dropdown/Dropdown';
-import Input from '@/components/ui/Input/Input';
 import { withAuth } from '@/components/auth/withAuth';
 
-function Page() {
-  const [question, setQuestion] = useState<string | undefined>(undefined);
-  const [name, setName] = useState('');
+import Button from "@/components/ui/Button/Button";
+import Input from "@/components/ui/Input/Input";
 
-  const options = [
-    { label: '배송 문의', value: 'delivery' },
-    { label: '환불 요청', value: 'refund' },
-    { label: '상품 문의', value: 'product' },
-    { label: '기타', value: 'etc' },
-  ];
+const labelStyle = `h-[45px] text-gray-500 py-2.25 px-3.75 flex gap-4 items-center justify-start rounded-[10px] bg-gray-100`;
 
+
+function MyPage() {
+
+  const handleSubmit = (data: any) => {
+    console.log(data);
+  };
+  
   return (
-    <div className="flex mt-2.5 gap-[32px]">
-      <div className="w-[300px] p-10">
-        <Dropdown
-          options={options}
-          value={question}
-          onChange={(v) => setQuestion(v)}
-          placeholder="문의 유형 선택"
-        />
+    <div className="flex flex-col items-center m-auto my-15">
+      <h1 className="text-2xl-semibold text-gray-500">계정 설정</h1>
+      <form
+        noValidate
+        className="w-84 md:w-100 flex flex-col gap-6 mt-12.5 relative"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col gap-[10px]">
+          <span className="text-md-regular text-gray-500">비밀번호 변경하기</span>
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="oldPassword"
+                className={`${labelStyle}`}
+              >
+                <Input
+                  type="password"
+                  id="oldPassword"
+                  placeholder="기존 비밀번호"
+                  className="w-full"
+                />
+              </label>
+              <label
+                htmlFor="newPassword"
+                className={`${labelStyle}`}
+              >
+                <Input
+                  type="password"
+                  id="newPassword"
+                  placeholder="새 비밀번호"
+                  className="w-full"
+                />
+                </label>
+              <label
+                htmlFor="newPasswordConfirm"
+                className={`${labelStyle}`}
+              >
+                <Input
+                  type="password"
+                  id="newPasswordConfirm"
+                  placeholder="새 비밀번호 확인"
+                  className="w-full"
+                />
+                </label>
+            </div>
+          </div>
+
+
+        <Button type="submit" disabled={false} fullWidth className="w-[89px] h-[40px] absolute bottom-0 right-0">
+          변경하기
+        </Button>
+      </form>
       </div>
-      <div className="p-10">
-        <Input
-          placeholder="이름을 입력하세요"
-          value={name}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-        />
-      </div>
-    </div>
   );
 }
-export default withAuth(Page);
+export default withAuth(MyPage);
