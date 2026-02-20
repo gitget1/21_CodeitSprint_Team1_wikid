c768d4ea79dbea3fc41582a6760d370568d1a557
zzungju <zzungju07@gmail.com>
2026-02-20 11:45:38 +0900

    :sparkles:Feat: 페이지 Title 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <title>WIKID</title>
+        <meta name="description" content="위키를 직접 작성하고 공유하는 소셜 네트워크 서비스" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
